80df81c484c8a96b3309a1ab050e8e53802cb95e
dante18 <anthony2.rossignol@gmail.com>
2024-03-11 19:21:32 +0100

    Install mentor(frontend) and niceadmin(backend) bootstrap template

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/assets/shared/js/app.js',
+                'resources/assets/frontend/js/app.js',
+                'resources/assets/backend/js/app.js'
             ],
             refresh: true,
         }),
-    ],
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'resources/assets/frontend/img',
+                    dest: 'assets/'
+                },
+                {
+                    src: 'resources/assets/backend/img',
+                    dest: 'assets/'
+                }
+            ]
+        })
+    ]
 });
