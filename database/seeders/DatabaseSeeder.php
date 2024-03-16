3ce7062fd026e4d7fd19e2ab2409709674e9c54c
dante18 <lablandinerie2@gmail.com>
2024-03-16 14:32:33 +0100

    Add model, factory, migration of parater table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Parameter;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Parameter::factory(1)->create([
+            'name' => 'APP_HOME_HERO_TITLE',
+            'value' => "Apprenez des compétences qui vous permettront d'évoluer"
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Parameter::factory(1)->create([
+            'name' => 'APP_HOME_HERO_SUB_TITLE',
+            'value' => 'de manière rapide et qui vous correspond'
+        ]);
     }
 }
