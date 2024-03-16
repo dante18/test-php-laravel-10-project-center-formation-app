c71028f68e3bd37bc764573075deb4c61251efcd
dante18 <lablandinerie2@gmail.com>
2024-03-16 14:34:12 +0100

    Add hero section in home page

--- app/Http/Controllers/Frontend/Home/HomeController.php
+++ app/Http/Controllers/Frontend/Home/HomeController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Frontend\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Parameter;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
     public function index(): View
     {
-        return view('frontend.home.index');
+        $heroTitle = Parameter::where('name', '=', 'APP_HOME_HERO_TITLE')->first()->value;
+        $heroSubTitle = Parameter::where('name', '=', 'APP_HOME_HERO_SUB_TITLE')->first()->value;
+
+        return view('frontend.home.index', [
+            'heroTitle' => $heroTitle,
+            'heroSubTitle' => $heroSubTitle
+        ]);
     }
 }
--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1,4 +1,18 @@
 @extends('index')
 
 @section('content')
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex justify-content-center align-items-center">
+        <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+            <h1>
+                {{ $heroTitle  }}
+            </h1>
+            <h2>
+                {{ $heroSubTitle  }}
+            </h2>
+            <a href="#" class="btn-get-started">
+                Démarrer mon inscription
+            </a>
+        </div>
+    </section><!-- End Hero -->
 @endsection
